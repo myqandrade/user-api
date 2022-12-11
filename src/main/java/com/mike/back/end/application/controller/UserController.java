294f429837fc206420d6ef35cd7633e4067df520
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-12-11 16:21:44 -0300

    Creating user service and refactoring user controller

--- src/main/java/com/mike/back/end/application/controller/UserController.java
+++ src/main/java/com/mike/back/end/application/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.mike.back.end.application.controller;
 
 import com.mike.back.end.application.dto.UserDto;
+import com.mike.back.end.application.service.UserService;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,77 +15,31 @@ public class UserController {
 
     public static List<UserDto> users = new ArrayList<>();
 
-    @PostConstruct
-    public void initiateList(){
-        UserDto user1 = new UserDto();
-        user1.setName("Mike");
-        user1.setCpf("132.094.956-03");
-        user1.setEmail("mike.andrade@hotmail.com");
-        user1.setPhone("21 98657-9387");
-        user1.setAddress("Vasco da Gama 1898");
-        user1.setRegistrationDate(new Date());
-
-        UserDto user2 = new UserDto();
-        user2.setName("Matheus");
-        user2.setCpf("145.798.976-04");
-        user2.setEmail("matheus.toledo@hotmail.com");
-        user2.setPhone("21 97650-3245");
-        user2.setAddress("Rua Brasil Mulambo 823");
-        user2.setRegistrationDate(new Date());
-
-        UserDto user3 = new UserDto();
-        user3.setName("Mariana");
-        user3.setCpf("267.553.984-05");
-        user3.setEmail("mariana.campos@hotmail.com");
-        user3.setPhone("21 96587-3247");
-        user3.setAddress("Rua Charles Menezes 231");
-        user3.setRegistrationDate(new Date());
-
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-    }
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/users")
-    public List<UserDto> getUsers(){
-        return users;
+    public List<UserDto> getUsers() {
+        return userService.getUsers();
     }
 
     @GetMapping("/users/{cpf}")
-    public UserDto getUserByCpf(@PathVariable(value="cpf") String cpf){
-        for(UserDto user : users){
-            if(user.getCpf().equals(cpf)){
-                return user;
-            }
-        }
-        return null;
+    public UserDto findByCpf(@PathVariable(value = "cpf") String cpf) {
+        return userService.findByCpf(cpf);
     }
 
-    @GetMapping("/")
-    public String getMessage(){
-        return "Spring boot is working!";
+    @GetMapping("/users/search")
+    public List<UserDto> queryByNameLike(@RequestParam(name="name", required = true) String name){
+        return userService.queryByNameLike(name);
     }
 
     @PostMapping("/newUser")
-    public UserDto createUser(@RequestBody UserDto userDto){
-        for(UserDto user : users){
-            if(user.getCpf().equals(userDto.getCpf())) {
-                return null;
-            }
-        }
-        userDto.setRegistrationDate(new Date());
-        users.add(userDto);
-        return userDto;
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return userService.createUser(userDto);
     }
 
-    @DeleteMapping("/users/{cpf}")
-    public boolean deleteUser(@PathVariable(value="cpf") String cpf){
-        for(UserDto user : users){
-            if(user.getCpf().equals(cpf)){
-                users.remove(user);
-                return true;
-            }
-        }
-        return false;
+    @DeleteMapping("/users/{id}")
+    public UserDto deleteUser(@PathVariable(value = "id") Long id) {
+        return userService.deleteUser(id);
     }
 }
---
+++ src/main/java/com/mike/back/end/application/service/UserService.java
@@ -0,0 +1,59 @@
+package com.mike.back.end.application.service;
+
+import com.mike.back.end.application.dto.UserDto;
+import com.mike.back.end.application.model.UserModel;
+import com.mike.back.end.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+
+    private UserRepository userRepository;
+
+    public List<UserDto> getUsers(){
+        List<UserModel> users = userRepository.findAll();
+        return users.stream().map(UserDto::convert).collect(Collectors.toList());
+    }
+
+    public UserDto findById(Long id){
+        Optional<UserModel> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return UserDto.convert(user.get());
+        }
+        return null;
+    }
+
+    public UserDto createUser(UserDto userDto){
+        userDto.setRegistrationDate(new Date());
+        UserModel user = userRepository.save(UserModel.convert(userDto));
+        return UserDto.convert(user);
+    }
+
+    public UserDto deleteUser(Long id){
+        Optional<UserModel> user = userRepository.findById(id);
+        if(user.isPresent()){
+            userRepository.delete(user.get());
+        }
+        return null;
+    }
+
+    public UserDto findByCpf(String cpf){
+        UserModel user = userRepository.findByCpf(cpf);
+        if(user != null){
+            return UserDto.convert(user);
+        }
+        return null;
+    }
+
+    public List<UserDto> queryByNameLike(String name){
+        List<UserModel> users = userRepository.queryByNameLike(name);
+        return users.stream().map(UserDto::convert).collect(Collectors.toList());
+    }
+}
